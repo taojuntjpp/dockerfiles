2d4009f560db74a1a31b5404f15614d6edb60630
taojun <taojuntjpp@126.com>
2016-04-26 20:10:49 +0800

    satellite

--- iqg-web/upload/start.sh
+++ iqg-web/upload/start.sh
@@ -4,12 +4,12 @@ initFile="${acePath}/run/imageInited"
 
 if [ -e ${initFile} ]
 then
-    cd /ace/code/iqgapi  
-    rm -rf /ace/code/iqgapi/app/logs
-    ln -s /ace/log/iqg /ace/code/iqgapi/app/logs
-    mkdir -p /ace/code/iqgapi/web/uploads/branches
-    mkdir /ace/code/iqgapi/web/uploads/brands
-    mkdir /ace/code/iqgapi/web/uploads/images
+    cd /ace/code/satellite  
+    rm -rf /ace/code/satellite/app/logs
+    ln -s /ace/log/iqg /ace/code/satellite/app/logs
+    mkdir -p /ace/code/satellite/web/uploads/branches
+    mkdir /ace/code/satellite/web/uploads/brands
+    mkdir /ace/code/satellite/web/uploads/images
 else
     mkdir -p  ${acePath}log/php  \
               ${acePath}log/nginx \
@@ -23,29 +23,29 @@ else
       mkdir -p /ace/log/iqg
     fi
 
-    if [ -d "/ace/code/iqgapi/app/logs" ]
+    if [ -d "/ace/code/satellite/app/logs" ]
     then
-      unlink /ace/code/iqgapi/app/logs
-      rm -rf /ace/code/iqgapi/app/logs
-      ln -s /ace/log/iqg /ace/code/iqgapi/app/logs
+      unlink /ace/code/satellite/app/logs
+      rm -rf /ace/code/satellite/app/logs
+      ln -s /ace/log/iqg /ace/code/satellite/app/logs
     fi
     touch ${initFile}
 fi
 
-\cp /ace/conf/nginx.conf /etc/nginx/nginx.conf
-\cp /ace/conf/wqy-microhei.ttc /ace/data/fonts/wqy-microhei.ttc
-\cp /ace/conf/authorized_keys /root/.ssh/authorized_keys
+\cp /ace/upload_conf/nginx.conf /etc/nginx/nginx.conf
+\cp /ace/upload_conf/wqy-microhei.ttc /ace/data/fonts/wqy-microhei.ttc
+\cp /ace/upload_conf/authorized_keys /root/.ssh/authorized_keys
 
 chown -R work:work /ace
 chown -R work:work /var/log/nginx
 chown -R work:work /var/lib/nginx
 
-su work -l -c"SYMFONY_ENV=prod /ace/code/iqgapi/app/console cache:clear"
+su work -l -c"SYMFONY_ENV=prod /ace/code/satellite/app/console cache:clear"
 
 /usr/sbin/nginx 
 su work -c -l "/usr/sbin/php-fpm -D"
 
-su work -l -c"\cp /ace/conf/authorized_keys /home/work/.ssh/authorized_keys"
+su work -l -c"\cp /ace/upload_conf/authorized_keys /home/work/.ssh/authorized_keys"
 su work -l -c"logrotate /etc/logrotate.d/nginx_log_rotate -s /ace/log/logrotate.status.tmp"
 su work -l -c"logrotate /etc/logrotate.d/php-fpm_log_rotate -s /ace/log/logrotate.status.tmp"
 
